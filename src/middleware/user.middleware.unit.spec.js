e55dda59263b5f98f4da4524564561b0b9832460
Felipe Ribeiro <piporibeiro@gmail.com>
2022-02-01 19:48:36 -0300

    refactor: use builders and separator methods

--- src/middleware/user.middleware.unit.spec.js
+++ src/middleware/user.middleware.unit.spec.js
@@ -2,22 +2,25 @@ import { appError } from "@/utils";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 import { get } from './user.middleware'
 import * as service from '@/database/service'
+import { buildNext, buildReq } from "test/builders";
 
 jest.mock('@/database/service')
 
 describe('Middleware > User', () =>  {
+  const error =  appError(
+    `${ReasonPhrases.UNPROCESSABLE_ENTITY}: header should contain a valid email`,
+    StatusCodes.UNPROCESSABLE_ENTITY,
+  );
+  
   afterEach(() => {
     jest.clearAllMocks()
   })
   
   it("should forward an error when an email is not provided in the headers", () => {
-    const req = {headers: {}};
-    const next = jest.fn().mockName('next')
-    const error =  appError(
-        `${ReasonPhrases.UNPROCESSABLE_ENTITY}: header should contain a valid email`,
-        StatusCodes.UNPROCESSABLE_ENTITY,
-      );
-    
+    const req = buildReq({
+      headers: {}
+    });
+    const next = buildNext()    
       get(req, null, next)
     
     expect(next).toHaveBeenCalledTimes(1)
@@ -25,14 +28,12 @@ describe('Middleware > User', () =>  {
   });
 
   it("should forward an error when an email is provided in the headers but is invalid", () => {
-    const req = {headers: {
-      email: 'email @email.com'
-      }};
-    const next = jest.fn().mockName('next')
-    const error =  appError(
-      `${ReasonPhrases.UNPROCESSABLE_ENTITY}: header should contain a valid email`,
-      StatusCodes.UNPROCESSABLE_ENTITY,
-    );
+    const req = buildReq({
+      headers: {
+        email: 'any @nay.com'
+      }
+    })
+    const next = buildNext()
 
     get(req, null, next)
 
@@ -41,36 +42,30 @@ describe('Middleware > User', () =>  {
   });
 
   it("should return an user object given a valid email is provided", async() => {
-    const email = 'email@email.com'
-    const req = {headers: {
-        email,
-      }};
-    
-    const next = jest.fn().mockName('next')
-    
-    jest.spyOn(service, 'findOrSave').mockResolvedValueOnce([{
+    const req = buildReq();
+
+    const next = buildNext()
+    const resolved = {
       id: 1,
-      email
-    }])
+      email: req.headers.email
+    }
+
+    jest.spyOn(service, 'findOrSave').mockResolvedValueOnce([resolved])
 
     await get(req, null, next)
     
     expect(req.user).toBeDefined()
-    expect(req.user).toEqual({
-      id: 1,
-      email
-    })
+    expect(req.user).toEqual(resolved)
     expect(next).toHaveBeenCalledTimes(1)
     expect(next).toHaveBeenCalledWith(/*nothing*/)
   });
 
   it("should forward an error when service findOrSave fails", async() => {
-    const email = 'email@email.com'
-    const req = {headers: {
-        email,
-      }};
+    const req = buildReq();
 
-    const next = jest.fn().mockName('next')
+    const next = buildNext()
+    
+    delete req.user
 
     jest.spyOn(service, 'findOrSave').mockRejectedValueOnce('any error')
 
