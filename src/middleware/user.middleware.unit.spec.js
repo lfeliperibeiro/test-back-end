34f7c4e0a0666dc88c7b729c372ab72f31462394
Felipe Ribeiro <piporibeiro@gmail.com>
2022-02-01 19:27:06 -0300

    test: forward an error when an email is provided in the headers but is invalid

--- src/middleware/user.middleware.unit.spec.js
+++ src/middleware/user.middleware.unit.spec.js
@@ -3,6 +3,10 @@ import { ReasonPhrases, StatusCodes } from "http-status-codes";
 import { get } from './user.middleware'
 
 describe('Middleware > User', () =>  {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+  
   it("should forward an error when an email is not provided in the headers", () => {
     const req = {headers: {}};
     const next = jest.fn().mockName('next')
@@ -13,6 +17,22 @@ describe('Middleware > User', () =>  {
     
       get(req, null, next)
     
+    expect(next).toHaveBeenCalledTimes(1)
+    expect(next).toHaveBeenCalledWith(error)
+  });
+
+  it("should forward an error when an email is provided in the headers but is invalid", () => {
+    const req = {headers: {
+      email: 'email @email.com'
+      }};
+    const next = jest.fn().mockName('next')
+    const error =  appError(
+      `${ReasonPhrases.UNPROCESSABLE_ENTITY}: header should contain a valid email`,
+      StatusCodes.UNPROCESSABLE_ENTITY,
+    );
+
+    get(req, null, next)
+
     expect(next).toHaveBeenCalledTimes(1)
     expect(next).toHaveBeenCalledWith(error)
   });
