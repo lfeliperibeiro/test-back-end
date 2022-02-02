e2f8f6ad179b4d4debb439d52596df6d973fc6d4
Felipe Ribeiro <piporibeiro@gmail.com>
2022-02-02 19:04:37 -0300

    test: call res status and res.json with proper data

---
+++ src/controllers/utils.unit.spec.js
@@ -0,0 +1,30 @@
+import {logger} from "@/utils";
+import {StatusCodes} from "http-status-codes";
+import {buildRes} from "test/builders";
+import {validationResponse} from "@/controllers/utils";
+
+jest.mock('@/utils')
+
+describe('Controller > utils', () => {
+  it("should call res.status and res.json with proper data", () => {
+    const res = buildRes()
+    const errors = {
+      array: jest.fn().mockReturnValueOnce(['error1', 'error2'])
+        .mockName('errors.array()'),
+    }
+    
+    validationResponse(res,errors)
+    
+    expect(logger.error).toHaveBeenCalledTimes(1)
+    expect(logger.error).toHaveBeenCalledWith('Validation failure', { 
+     errors: ['error1', 'error2'] ,
+     })
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.status).toHaveBeenCalledWith(StatusCodes.UNPROCESSABLE_ENTITY)
+    expect(res.json).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith( {
+      errors: ['error1', 'error2'] ,
+    })
+    expect(errors.array).toHaveBeenCalledTimes(1)
+  });
+})
