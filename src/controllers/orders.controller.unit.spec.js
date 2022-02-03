0f431dfaaaafc19efeb4e7ad15375ae35b8a801c
Felipe Ribeiro <piporibeiro@gmail.com>
2022-02-03 18:19:22 -0300

    test: forward an error when service.listOrder fails

--- src/controllers/orders.controller.unit.spec.js
+++ src/controllers/orders.controller.unit.spec.js
@@ -1,5 +1,6 @@
-import { buildNext, buildOrders, buildReq, buildRes } from "test/builders";
+import { buildError, buildNext, buildOrders, buildReq, buildRes } from "test/builders";
 import {index} from './orders.controller';
+import { StatusCodes } from "http-status-codes";
 
 describe('Controllers > Orders', () => {
   it("should return status 200 with a list of orders", async () => { 
@@ -23,7 +24,25 @@ describe('Controllers > Orders', () => {
   });
 
   it("should forward an error when service.listOrder fails", async () => {
+    const req = buildReq();
+    const res = buildRes()
+    const next = buildNext()
+    const error = buildError(
+      StatusCodes.INTERNAL_SERVER_ERROR,
+      'Some message here'
+    )
 
+    jest.spyOn(req.service, 'listOrders').mockRejectedValueOnce(error)
+
+    await index(req, res, next)
+
+    expect(res.status).not.toHaveBeenCalled()
+    expect(res.status).not.toHaveBeenCalled()
+
+    expect(next).toHaveBeenCalledTimes(1)
+    expect(next).toHaveBeenCalledWith(error)
+
+    
   });
   
 })
