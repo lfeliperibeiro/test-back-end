b0f1293b417714887481eb1ca38cc3d7879f9821
Felipe Ribeiro <piporibeiro@gmail.com>
2022-02-07 19:33:48 -0300

    test: return status 500 and an error message when listOrder rejects

--- src/routes/orders.router.integration.spec.js
+++ src/routes/orders.router.integration.spec.js
@@ -1,6 +1,7 @@
 import * as service from '@/database/service';
-import { buildOrders } from "test/builders";
+import { buildError, buildOrders } from "test/builders";
 import { buildCall } from "test/builders.integration";
+import { StatusCodes } from "http-status-codes";
 
 
 jest.mock('@/database/service')
@@ -17,5 +18,19 @@ describe('Router > Integration > Orders', () => {
     expect(res.body).toEqual({ orders })
     
    done();
+  });
+
+  it("should return status 500 and an error message when listOrder rejects", async done => {
+    const error = buildError(StatusCodes.INTERNAL_SERVER_ERROR,
+      'Failed to retrieve list of orders')
+
+    jest.spyOn(service, 'listOrders').mockRejectedValueOnce(error)
+
+    const res = await buildCall('/api/order')
+
+    expect(res.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
+    expect(res.body).toEqual({ message: 'Failed to retrieve list of orders' })
+
+    done();
   });
 })
